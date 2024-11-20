337d95eb64199046a2e9494d6e74dd490a687cea
aragogelsabio <nopuedoestoydormido@gmail.com>
2024-11-20 01:30:18 -0500

    Actualicé el programa FizzBuzz con un else if

--- FizzBuzzHechoPorMi/FizzBuzz.java
+++ FizzBuzzHechoPorMi/FizzBuzz.java
@@ -21,14 +21,11 @@ public static void main(String[] args) {
  for(int i=1; i<=100; i++){
     if(i % 3 == 0 && i%5 == 0){
         System.out.println("FizzBuzz");
-    }
-    if(i % 3 == 0){
+    } else if(i % 3 == 0){
         System.out.println("Fizz");
-    }
-    if(i % 5 == 0){
+    } else if(i % 5 == 0){
         System.out.println("Buzz");
-    }
-    if(i % 3 !=0 && i % 5 !=0){
+    } else {
         System.out.println(i);
     }
  }
